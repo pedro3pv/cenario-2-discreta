a1e989130cffb93cff7362c5af4d7ba24c1a04f5
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2023-10-15 14:05:57 -0300

    iguais
    a  criei a função responsável por separar os itens que contem em ambas as data bases e armazená-los

--- DB/DB.py
+++ DB/DB.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib
 
 
 def repetidos(DB1, DB2):
@@ -11,7 +12,8 @@ def repetidos(DB1, DB2):
 
     while x < int(LengthDB1):
         for y in range(int(LengthDB2)):
-            if DB1["Nome"][x] == DB2["Nome"][y] and DB1["Nome da Mae"][x] == DB2["Nome da Mae"][y] and DB1["Nome do Pai"][x] == DB2["Nome do Pai"][y] and DB1["Sexo"][x].upper() == DB2["Sexo"][y]:
+            if DB1["Nome"][x] == DB2["Nome"][y] and DB1["Nome da Mae"][x] == DB2["Nome da Mae"][y] and \
+                    DB1["Nome do Pai"][x] == DB2["Nome do Pai"][y] and DB1["Sexo"][x].upper() == DB2["Sexo"][y]:
                 print("DB1:" + str(x))
                 print("DB2:" + str(y) + "\n")
                 print("DB1:" + DB1["Nome"][x])
@@ -28,21 +30,25 @@ def repetidos(DB1, DB2):
     DB1.reset_index(drop=True, inplace=True)
     return DB1
 
-def iguais(DB1,DB2):
-    LengthDB1 = DB1.shape[0]
-    LengthDB2 = DB2.shape[0]
 
-    loop = False
-    x = 0
-    y = 0
+def iguais(DB1, DB2):
+    # Crie uma coluna temporária em cada DataFrame com "Sexo" em maiúsculas
+    DB1["Sexo"] = DB1["Sexo"].str.upper()
+    DB2["Sexo"] = DB2["Sexo"].str.upper()
 
-    while x < int(LengthDB1):
-        for y in range(int(LengthDB2)):
-            if DB1["Nome"][x] == DB2["Nome"][y] and DB1["Nome da Mae"][x] == DB2["Nome da Mae"][y] and DB1["Nome do Pai"][x] == DB2["Nome do Pai"][y] and DB1["Sexo"][x].upper() == DB2["Sexo"][y]:
-                #DB1 = DB1.
-                pass
-    DB1.reset_index(drop=True, inplace=True)
-    return DB1
+    chave = ["Nome", "Nome da Mae", "Nome do Pai", "Sexo"]
+
+    DB_iguais = pd.merge(DB1, DB2, on=chave, how='inner')
+
+    DB_iguais.drop(list(DB_iguais.filter(regex='_y')), axis=1, inplace=True)
+    DB_iguais = DB_iguais.rename(columns={
+        "ID_x": "ID",
+        "Ônibus_x": "Ônibus",
+        "Data da Dengue_x": "Data da Dengue",
+        "Data de Nascimento_x": "Data de Nascimento",
+    })
+
+    return DB_iguais
 
 
 def adicionar(DB1, DB2):
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ baseDeDengue = pd.read_csv("DB/BaseDengue7.csv")
 baseDeOnibus = pd.read_csv("DB/BaseOnibus7.csv", encoding="cp1252")
 #baseDeCidadoes = DB.adicionar(DB.adicionar(baseDeAlunos, baseDeDengue), baseDeOnibus)
 
-relatorioEducacao = DB.repetidos(baseDeAlunos, baseDeDengue)
-relatorioEducacao[["Nome", "Data de Nascimento", "ID"]].to_excel("Relatório Educação.xlsx")
+#relatorioEducacao = DB.repetidos(baseDeAlunos, baseDeDengue)
+#relatorioEducacao[["Nome", "Data de Nascimento", "ID"]].to_excel("Relatório Educação.xlsx")
+
+test = DB.iguais(baseDeAlunos, baseDeDengue)
+print(test)
+test[["Nome", "Data de Nascimento", "Data da Dengue"]].to_excel("test.xlsx")
 
 #BaseNaoOnibus = DB.repetidos(baseDeDengue, baseDeOnibus)
 #relatorioSaude = DB.repetidos(baseDeDengue, BaseNaoOnibus)
@@ -17,3 +21,5 @@ relatorioMobilidade = DB.repetidos(baseDeOnibus, baseDeDengue)
 relatorioMobilidade[["Nome", "Data de Nascimento", "Ônibus"]].to_excel("Relatório Mobilidade.xlsx")
 
 #RelatorioEducacaoeSaude =
+
+#RelatorioEducacaoeMobilidade =
